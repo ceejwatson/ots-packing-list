4d42512c137363fe74d70637ad4a2e1925f76e90
CJ Watson <cjwatson@CJs-MacBook-Air.local>
2025-11-07 09:41:45 -0600

    Update sitemap and robots.txt with new domain otspackinglist.com

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,26 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://ots-packing-list.vercel.app'
+  const baseUrl = "https://otspackinglist.com";
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
       url: `${baseUrl}/faqs`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/reporting`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.9,
     },
-  ]
+  ];
 }
